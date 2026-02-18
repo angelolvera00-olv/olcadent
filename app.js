a9907c2797ef5c05eecf0b4c253963845e19108a
Angel <angelolvera00@gmail.com>
2026-02-18 01:31:39 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const ejsMate = require('ejs-mate')
+
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('ejs', ejsMate)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Escuchando al servidor 3000")
+});
